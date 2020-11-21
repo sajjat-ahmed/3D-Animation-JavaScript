efcce37c547fb5f6d19b9cbe99f9c56451ae8018
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-11-21 17:55:41 +0600

    Javascript 3D Animation

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 
+// Items
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+
 // Moving Animation Event
 container.addEventListener('mousemove', (e) => {
     let xAxix = (window.innerWidth / 2 - e.pageX) / 25;
@@ -10,9 +17,20 @@ container.addEventListener('mousemove', (e) => {
 })
 
 // Animation In
+container.addEventListener('mouseenter', (e) => {
+    card.style.transition = "none";
 
+    //Popout
+    title.style.transform = "translateZ(150px)";
+    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+})
 
 // Animation Out
 container.addEventListener('mouseleave', (e) => {
+    card.style.transition = "all 0.5s ease";
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+    // Popback
+    title.style.transform = "translateZ(0px)";
+    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
 })
